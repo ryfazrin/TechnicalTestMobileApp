c83451f6f973e98c90346aa7f7f47ccf54008fe1
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2022-01-04 00:58:37 +0800

    update: ambil data dari MainActivity di DetailPostActivity

--- app/src/main/java/com/ryfazrin/technicaltestmobileapp/ui/detailpost/DetailPostActivity.kt
+++ app/src/main/java/com/ryfazrin/technicaltestmobileapp/ui/detailpost/DetailPostActivity.kt
@@ -2,10 +2,31 @@ package com.ryfazrin.technicaltestmobileapp.ui.detailpost
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ryfazrin.technicaltestmobileapp.R
+import com.ryfazrin.technicaltestmobileapp.data.DetailUserResponse
+import com.ryfazrin.technicaltestmobileapp.data.PostsResponseItem
+import com.ryfazrin.technicaltestmobileapp.databinding.ActivityDetailPostBinding
 
 class DetailPostActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailPostBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail_post)
+        binding = ActivityDetailPostBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val getPost: PostsResponseItem = intent.getParcelableExtra<PostsResponseItem>(EXTRA_POST) as PostsResponseItem
+        val getUser: DetailUserResponse = intent.getParcelableExtra<DetailUserResponse>(
+            EXTRA_DETAIL_USER) as DetailUserResponse
+
+        binding.tvDetailPostTitle.text = getPost.title
+        binding.tvDetailPostName.text = getUser.name
+        binding.tvDetailPostBody.text = getPost.body
+    }
+
+    companion object {
+        var EXTRA_POST = "extra_post"
+        var EXTRA_DETAIL_USER = "extra_detail_user"
     }
 }
