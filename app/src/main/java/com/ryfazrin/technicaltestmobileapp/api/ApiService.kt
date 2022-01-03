aa8e5092b8ca1e0de84d90e612e4adecc486e364
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2022-01-04 00:03:29 +0800

    update: tambah request api detail user di ApiService

--- app/src/main/java/com/ryfazrin/technicaltestmobileapp/api/ApiService.kt
+++ app/src/main/java/com/ryfazrin/technicaltestmobileapp/api/ApiService.kt
@@ -1,10 +1,17 @@
 package com.ryfazrin.technicaltestmobileapp.api
 
+import com.ryfazrin.technicaltestmobileapp.data.DetailUserResponse
 import com.ryfazrin.technicaltestmobileapp.data.PostsResponseItem
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface ApiService {
     @GET("posts")
     fun getPosts(): Call<List<PostsResponseItem>>
+
+    @GET("users/{userId}")
+    fun getDetailUser(
+        @Path("userId") userId: Int
+    ) : Call<DetailUserResponse>
 }
