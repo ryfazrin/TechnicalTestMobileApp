2f6937eb16d2f0db69df9ee12bed2adc679c6492
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2022-01-04 23:45:13 +0800

    fix: load more paging MainActivity

--- app/src/main/java/com/ryfazrin/technicaltestmobileapp/ui/main/MainActivity.kt
+++ app/src/main/java/com/ryfazrin/technicaltestmobileapp/ui/main/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.ryfazrin.technicaltestmobileapp.ui.main
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -33,9 +36,8 @@ class MainActivity : AppCompatActivity() {
         listPost = ArrayList<PostsResponseItem>()
 
         mainViewModel.posts.observe(this, { posts ->
-
             if (listPost.isEmpty()) {
-                setPostData(posts, mainViewModel.listUser)
+                setPostData(posts, mainViewModel.listUser, 0)
             }
 
             val scrollDirectionDown = 1 // Scroll down is +1, up is -1.
@@ -52,16 +54,25 @@ class MainActivity : AppCompatActivity() {
                         // List has more item.
                         if (currentListSize != listSizeAfterLoading) {
                             currentListSize = listSizeAfterLoading
+                            Log.e("MainActivity", "listSizeAfterLoading: $listSizeAfterLoading")
+                            Log.e("MainActivity", "currentListSize: $currentListSize")
+
                             showLoading(true)
-                            // Get more posts.
-                            if (listPost.isNotEmpty()) {
+                            if (listPost.size == posts.size) {
+                                showLoading(false)
+                            } else {
+                                Log.e("MainActivity", "listPost.size: ${listPost.size}")
                                 setPostData(posts, mainViewModel.listUser, listPost.size)
                             }
                             showLoading(false)
                         }
                         else { // List comes limit.
-//                            showToastMessageShort("No more items.")
-                            showError(true)
+                            showLoading(false)
+                            val text = "No more items!"
+                            val duration = Toast.LENGTH_SHORT
+
+                            val toast = Toast.makeText(applicationContext, text, duration)
+                            toast.show()
                         }
                     }
                 }
@@ -85,14 +96,9 @@ class MainActivity : AppCompatActivity() {
 
     private fun setPostData(posts: List<PostsResponseItem>, users: ArrayList<DetailUserResponse>, listLength: Int = 0) {
 //        listPost.clear()
-
         var counter = listLength
-//
-//        if (listPost.size != null) {
-//            counter = listPost.size
-//        }
 
-        while (counter < counter + 5){
+        while (counter < listLength + 10){
             listPost.add(posts[counter])
             counter++
         }
@@ -108,26 +114,8 @@ class MainActivity : AppCompatActivity() {
                 showSelectedPost(post, user)
             }
         })
-    }
 
-//    private fun loadMore(posts: List<PostsResponseItem>, users: ArrayList<DetailUserResponse>) {
-//        var counter = listPost.size
-//        while (counter < counter + 5) {
-//            listPost.add(posts[counter])
-//            counter++
-//        }
-//
-//
-//        val adapter = MainPostsAdapter(listPost, users)
-//
-//        binding.rvPosts.adapter = adapter
-//
-//        adapter.setOnItemClickCallback(object : MainPostsAdapter.OnItemCLickCallback {
-//            override fun onItemClicked(post: PostsResponseItem, user: DetailUserResponse) {
-//                showSelectedPost(post, user)
-//            }
-//        })
-//    }
+    }
 
     private fun showSelectedPost(post: PostsResponseItem, user: DetailUserResponse) {
         val moveDetailIntent = Intent(this@MainActivity, DetailPostActivity::class.java)
