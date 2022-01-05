f246121d40a329b824fbf1b85dc74a993b4a45d8
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2022-01-05 14:27:28 +0800

    update: RecyclerView photos di AlbumsAdapter

--- app/src/main/java/com/ryfazrin/technicaltestmobileapp/ui/detailuser/AlbumsAdapter.kt
+++ app/src/main/java/com/ryfazrin/technicaltestmobileapp/ui/detailuser/AlbumsAdapter.kt
@@ -3,29 +3,37 @@ package com.ryfazrin.technicaltestmobileapp.ui.detailuser
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.ryfazrin.technicaltestmobileapp.data.AlbumsResponseItem
 import com.ryfazrin.technicaltestmobileapp.data.PhotosResponseItem
 import com.ryfazrin.technicaltestmobileapp.databinding.ItemRowAlbumBinding
-import com.ryfazrin.technicaltestmobileapp.ui.detailpost.CommentsViewModel
+import kotlinx.coroutines.DelicateCoroutinesApi
 
 //class AlbumsAdapter(private val listAlbum: ArrayList<AlbumsResponseItem>) : RecyclerView.Adapter<AlbumsAdapter.ListViewHolder>()  {
-class AlbumsAdapter(private val listAlbum: ArrayList<AlbumsResponseItem>, private val listPhotos: ArrayList<List<PhotosResponseItem>>) : RecyclerView.Adapter<AlbumsAdapter.ListViewHolder>()  {
+@DelicateCoroutinesApi
+class AlbumsAdapter(
+    private val listAlbum: ArrayList<AlbumsResponseItem>,
+    private val listPhotos: ArrayList<List<PhotosResponseItem>>,
+    private val detailUserActivity: DetailUserActivity
+) : RecyclerView.Adapter<AlbumsAdapter.ListViewHolder>()  {
 
     inner class ListViewHolder(private val binding: ItemRowAlbumBinding) : RecyclerView.ViewHolder(binding.root) {
 
 //        fun bind(album: AlbumsResponseItem) {
-        fun bind(album: AlbumsResponseItem, photos: List<PhotosResponseItem>) {
+        fun bind(
+    album: AlbumsResponseItem,
+    photos: List<PhotosResponseItem>,
+    detailUserActivity: DetailUserActivity
+) {
 
             with(binding) {
                 tvTitleAlbum.text = album.title
-//                val layoutManager = LinearLayoutManager(this@AlbumsAdapter)
-//                rvAlbumPhotos.layoutManager = layoutManager
+                val layoutManager = LinearLayoutManager(detailUserActivity)
+                rvAlbumPhotos.layoutManager = layoutManager
                 val adapter = PhotosAdapter(photos)
                 rvAlbumPhotos.adapter = adapter
-//                Log.e("AlbumsAdapter", "bind: $photos")
+                Log.e("AlbumsAdapter", "bind: $photos")
             }
         }
     }
@@ -37,7 +45,7 @@ class AlbumsAdapter(private val listAlbum: ArrayList<AlbumsResponseItem>, privat
 
     override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
 //        holder.bind(listAlbum[position])
-        holder.bind(listAlbum[position], listPhotos[position])
+        holder.bind(listAlbum[position], listPhotos[position], detailUserActivity)
     }
 
     override fun getItemCount(): Int = listAlbum.size
