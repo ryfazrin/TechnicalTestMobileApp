867e15449515fbc61fc14c0c2f79fb403ec189f4
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2022-01-05 23:09:47 +0800

    fix bug: Membuat nested-recyclerview dengan dummy data
    https://medium.com/nerd-for-tech/nested-recyclerview-in-android-e5afb2b9771a

--- app/src/main/java/com/ryfazrin/technicaltestmobileapp/ui/detailuser/AlbumsAdapter.kt
+++ app/src/main/java/com/ryfazrin/technicaltestmobileapp/ui/detailuser/AlbumsAdapter.kt
@@ -2,8 +2,9 @@ package com.ryfazrin.technicaltestmobileapp.ui.detailuser
 
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.LiveData
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.ryfazrin.technicaltestmobileapp.data.AlbumsResponseItem
 import com.ryfazrin.technicaltestmobileapp.data.PhotosResponseItem
@@ -17,26 +18,40 @@ import kotlinx.coroutines.runBlocking
 @DelicateCoroutinesApi
 class AlbumsAdapter(
     private val listAlbum: ArrayList<AlbumsResponseItem>,
-    private val listPhotos: List<PhotosResponseItem>,
-    private val detailUserActivity: DetailUserActivity
+    private val listPhotos: ArrayList<List<PhotosResponseItem>>
 ) : RecyclerView.Adapter<AlbumsAdapter.ListViewHolder>()  {
 
     inner class ListViewHolder(private val binding: ItemRowAlbumBinding) : RecyclerView.ViewHolder(binding.root) {
 
 //        fun bind(album: AlbumsResponseItem) {
         fun bind(
-    album: AlbumsResponseItem,
-    photos: PhotosResponseItem,
-    detailUserActivity: DetailUserActivity
-) {
-
+        album: AlbumsResponseItem
+//        photos: List<PhotosResponseItem>
+    ) {
+        val photoDummy = listOf<PhotosResponseItem>(
+            PhotosResponseItem(
+                1,
+                100,
+                "photo satu",
+                "ini url satu",
+                "https://via.placeholder.com/150/8e973b"
+            ),
+            PhotosResponseItem(
+                2,
+                200,
+                "photo dua",
+                "ini url dua",
+                "https://via.placeholder.com/150/121fa4"
+            )
+        )
             with(binding) {
                 tvTitleAlbum.text = album.title
-                tvTitlePhotos.text = "${photos.id}. ${photos.title}"
-//                val layoutManager = LinearLayoutManager(detailUserActivity)
-//                rvAlbumPhotos.layoutManager = layoutManager
-//                val adapter = PhotosAdapter(photos)
-//                rvAlbumPhotos.adapter = adapter
+//                tvTitlePhotos.text = "${photos.id}. ${photos.title}"
+                val layoutManager = LinearLayoutManager(itemView.context, LinearLayoutManager.VERTICAL, false)
+//                val layoutManager = LinearLayoutManager(binding.root.context, LinearLayoutManager.VERTICAL, false)
+                rvAlbumPhotos.layoutManager = layoutManager
+                val adapter = PhotosAdapter(photoDummy)
+                rvAlbumPhotos.adapter = adapter
             }
         }
     }
@@ -54,8 +69,9 @@ class AlbumsAdapter(
 
         runBlocking {
             jobListPhotos.join()
-            Log.e("AlbumsAdapter", "onBindViewHolder $position: ${listPhotos[1]}")
-            holder.bind(listAlbum[position], listPhotos[position], detailUserActivity)
+//            Log.e("AlbumsAdapter", "onBindViewHolder $position: ${listPhotos[1]}")
+//            holder.bind(listAlbum[position], listPhotos[position])
+            holder.bind(listAlbum[position])
         }
     }
 
