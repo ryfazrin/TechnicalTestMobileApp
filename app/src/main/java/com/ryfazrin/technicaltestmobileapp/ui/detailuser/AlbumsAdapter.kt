d60a25424c4023206f178c18f8518678887637e8
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2022-01-05 13:00:53 +0800

    update error: masukkan listPhoto ke dalam rv di AlbumsAdapter
    error tidak tampil data listPhoto pada recyclerview karena permasalahan layoutManager

--- app/src/main/java/com/ryfazrin/technicaltestmobileapp/ui/detailuser/AlbumsAdapter.kt
+++ app/src/main/java/com/ryfazrin/technicaltestmobileapp/ui/detailuser/AlbumsAdapter.kt
@@ -1,17 +1,31 @@
 package com.ryfazrin.technicaltestmobileapp.ui.detailuser
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.ryfazrin.technicaltestmobileapp.data.AlbumsResponseItem
+import com.ryfazrin.technicaltestmobileapp.data.PhotosResponseItem
 import com.ryfazrin.technicaltestmobileapp.databinding.ItemRowAlbumBinding
+import com.ryfazrin.technicaltestmobileapp.ui.detailpost.CommentsViewModel
 
-class AlbumsAdapter(private val listAlbum: ArrayList<AlbumsResponseItem>) : RecyclerView.Adapter<AlbumsAdapter.ListViewHolder>()  {
+//class AlbumsAdapter(private val listAlbum: ArrayList<AlbumsResponseItem>) : RecyclerView.Adapter<AlbumsAdapter.ListViewHolder>()  {
+class AlbumsAdapter(private val listAlbum: ArrayList<AlbumsResponseItem>, private val listPhotos: ArrayList<List<PhotosResponseItem>>) : RecyclerView.Adapter<AlbumsAdapter.ListViewHolder>()  {
 
     inner class ListViewHolder(private val binding: ItemRowAlbumBinding) : RecyclerView.ViewHolder(binding.root) {
-        fun bind(album: AlbumsResponseItem) {
+
+//        fun bind(album: AlbumsResponseItem) {
+        fun bind(album: AlbumsResponseItem, photos: List<PhotosResponseItem>) {
+
             with(binding) {
                 tvTitleAlbum.text = album.title
+//                val layoutManager = LinearLayoutManager(this@AlbumsAdapter)
+//                rvAlbumPhotos.layoutManager = layoutManager
+                val adapter = PhotosAdapter(photos)
+                rvAlbumPhotos.adapter = adapter
+//                Log.e("AlbumsAdapter", "bind: $photos")
             }
         }
     }
@@ -22,7 +36,8 @@ class AlbumsAdapter(private val listAlbum: ArrayList<AlbumsResponseItem>) : Recy
     }
 
     override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
-        holder.bind(listAlbum[position])
+//        holder.bind(listAlbum[position])
+        holder.bind(listAlbum[position], listPhotos[position])
     }
 
     override fun getItemCount(): Int = listAlbum.size
